36377e1d94a476874f49d26003631e2f2df33a73
bedirhaneren <163941707+bedirhaneren@users.noreply.github.com>
2025-08-14 23:32:02 +0300

    Frontend taslağı ve hata düzeltmeleri

--- models/User.js
+++ models/User.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
-  avatar: {type: String , required : Option},
-  bio : {type: String , required : Option},
+avatar: { type: String, required: false }, 
+bio: { type: String, required: false }  ,
   role: {type : String, enum : ['user' , 'mod' , 'admin'], default : "user"},
   followers: {type : mongoose.Schema.ObjectId},
   following: {type: mongoose.Schema.ObjectId}
