5febbf856a1c84b56e7ce8a0704ca79839f9022c
bedirhaneren <163941707+bedirhaneren@users.noreply.github.com>
2025-08-17 19:44:05 +0300

    Jwt auth düzenledim

--- services/authMiddleware.js
+++ services/authMiddleware.js
@@ -2,14 +2,15 @@ const jwt = require("jsonwebtoken") ;
 
 
 function authMiddleware(req, res, next) {
-    const token = req.header('Authorization')?.split(' ')[1]; // "Bearer token" formatından al
+    const token = req.header('Authorization')?.split(' ')[1]; // Bearer token
     if (!token) return res.status(401).json({ message: 'Token yok, giriş yapın' });
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // Token içindeki bilgileri req.user'a ekle
+        const decoded = jwt.verify(token, "gizli-key"); 
+        req.user = decoded; // 
         next();
     } catch (err) {
+        console.error('Token doğrulama hatası:', err);
         res.status(401).json({ message: 'Geçersiz veya süresi dolmuş token' });
     }
 }
