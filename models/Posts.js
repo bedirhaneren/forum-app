c34ce942a0cd840e68eb71925d9c34befb80f0a6
bedirhaneren <163941707+bedirhaneren@users.noreply.github.com>
2025-08-17 19:44:53 +0300

    Post schema düzelttim

--- models/Posts.js
+++ models/Posts.js
@@ -4,15 +4,20 @@ const User = require('./User');
 const postSchema = new mongoose.Schema({
     title : {type : String, required : true }, 
     content : {type: String, required: true} ,
-    authorId : {type : mongoose.Schema.ObjectId , ref: 'User', required: true} ,
-    authorUsername : {type: String , required : true} ,
-    likes: {type : mongoose.Schema.ObjectId , ref : 'User'} ,
-    dislikes : {type : mongoose.Schema.ObjectId, ref : 'User'},
-    views : {type: Number},
+    category : {type : String, required : true}, 
+    
+    authorId : {type : mongoose.Schema.ObjectId , ref: 'User', required : true} , 
+    authorUsername : {type : String , required : true} , 
+    likes: [{type : mongoose.Schema.ObjectId , ref : 'User'}],
+    
+    dislikes : [{type : mongoose.Schema.ObjectId, ref : 'User'}], 
+    
+    views : {type: Number, default: 0}, 
     tags: {type: String},
     createdAt : {type : Date, default: Date.now},
     updatedAt : {type : Date , default : Date.now}
 
 }, {timestamps: true}) ;
 
-module.exports= mongoose.model('Post', postSchema) ; 
+module.exports= mongoose.model('Post', postSchema, 'posts') ;
+
